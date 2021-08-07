dac05cef0c6b36afade8823abf538ac8d7f2c0f2
Ely Saakian <ely.saakian@gmail.com>
2021-08-07 12:07:40 -0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Container } from "@material-ui/core";
 import WeatherPage from "./pages/WeatherPage";
 import { makeStyles } from "@material-ui/core/styles";
 import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const useStyles = makeStyles({
   container: {
@@ -13,8 +14,16 @@ export default function App() {
   const classes = useStyles();
   return (
     <Container disableGutters className={classes.container}>
-      <HomePage />
-      {/* <WeatherPage /> */}
+      <Router>
+        <Switch>
+          <Route path="/weather">
+            <WeatherPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </Container>
   );
 }
