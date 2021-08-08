54170d4189383205b21dc90ddcfadc7c15b783bd
Ely Saakian <ely.saakian@gmail.com>
2021-08-07 22:58:38 -0700

    add route transitions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { Container } from "@material-ui/core";
 import WeatherPage from "./pages/WeatherPage";
 import { makeStyles } from "@material-ui/core/styles";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import OpenWeatherClient from "./api/OpenWeatherClient";
+import { CSSTransition } from "react-transition-group";
+import "./App.css";
 
 const useStyles = makeStyles({
   container: {
@@ -10,19 +13,34 @@ const useStyles = makeStyles({
   },
 });
 
+const routes = [
+  { path: "/", name: "Home", Component: HomePage },
+  { path: "/weather", name: "Weather", Component: WeatherPage },
+];
+
+const client = new OpenWeatherClient("a3345aba730886077c25b145c5518740");
+
 export default function App() {
   const classes = useStyles();
   return (
     <Container disableGutters className={classes.container}>
       <Router>
-        <Switch>
-          <Route path="/weather">
-            <WeatherPage />
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path}>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                timeout={300}
+                classNames="page"
+                unmountOnExit
+              >
+                <div className="page">
+                  <Component />
+                </div>
+              </CSSTransition>
+            )}
           </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
+        ))}
       </Router>
     </Container>
   );
