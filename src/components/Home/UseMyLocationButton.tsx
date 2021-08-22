8a31dce0384141fd7e0562d7d78a63ca1874afaf
Ely Saakian <ely.saakian@gmail.com>
2021-08-21 17:41:23 -0700

    add get weather by current location

---
+++ src/components/Home/UseMyLocationButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@material-ui/core";
+import { MyLocation } from "@material-ui/icons";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import WeatherContext from "../../store/weather-context";
+
+export default function UseMyLocationButton() {
+	const weatherCtx = useContext(WeatherContext);
+	const history = useHistory();
+
+	const useMyLocationHandler = () => {
+		navigator.geolocation.getCurrentPosition(({ coords }) => {
+			weatherCtx.weatherClient
+				.getWeatherForCityByCoords(coords.longitude, coords.latitude)
+				.then((data) => weatherCtx.setWeatherData(data))
+				.then(() => history.push("/weather"));
+		});
+	};
+
+	return (
+		<Button type="submit" color="primary" disableElevation endIcon={<MyLocation />} onClick={useMyLocationHandler}>
+			Use my location
+		</Button>
+	);
+}
