356d727d650738824e71424d4a37cf8e684425bc
Ely Saakian <ely.saakian@gmail.com>
2021-08-21 17:42:16 -0700

    refactor weather context to provide weather client

--- src/components/Home/SearchButton.tsx
+++ src/components/Home/SearchButton.tsx
@@ -2,32 +2,29 @@ import { Button } from "@material-ui/core";
 import { Search } from "@material-ui/icons";
 import { useContext } from "react";
 import { useHistory } from "react-router-dom";
-import OpenWeatherClient from "../../api/OpenWeatherClient";
 import WeatherContext from "../../store/weather-context";
 
-const client = new OpenWeatherClient("a3345aba730886077c25b145c5518740");
-
 export default function SearchButton() {
-  const weatherCtx = useContext(WeatherContext);
-  const history = useHistory();
+	const weatherCtx = useContext(WeatherContext);
+	const history = useHistory();
 
-  const clickHandler = () => {
-    client
-      .getWeatherForCityById(weatherCtx.selectedCityId)
-      .then((data) => weatherCtx.setWeatherData(data))
-      .then(() => history.push("/weather"));
-  };
+	const clickHandler = () => {
+		weatherCtx.weatherClient
+			.getWeatherForCityById(weatherCtx.selectedCityId || "")
+			.then((data) => weatherCtx.setWeatherData(data))
+			.then(() => history.push("/weather"));
+	};
 
-  return (
-    <Button
-      type="submit"
-      variant="contained"
-      color="primary"
-      disableElevation
-      onClick={clickHandler}
-      endIcon={<Search />}
-    >
-      Go
-    </Button>
-  );
+	return (
+		<Button
+			type="submit"
+			variant="contained"
+			color="primary"
+			disableElevation
+			onClick={clickHandler}
+			endIcon={<Search />}
+		>
+			Go
+		</Button>
+	);
 }
--- src/store/weather-context.tsx
+++ src/store/weather-context.tsx
@@ -1,42 +1,40 @@
 import { Context, ReactNode, useState } from "react";
 import { createContext } from "react";
-import { IWeather } from "../api/OpenWeatherClient";
+import OpenWeatherClient, { IWeather } from "../api/OpenWeatherClient";
+
+const weatherClient = new OpenWeatherClient("a3345aba730886077c25b145c5518740");
 
 const WeatherContext: Context<{
-  weatherData: IWeather;
-  selectedCityId: string;
-  setSelectedCityId: (cityId: string) => void;
-  setWeatherData: (weatherData: IWeather) => void;
+	weatherClient: OpenWeatherClient;
+	weatherData?: IWeather;
+	selectedCityId?: string;
+	setSelectedCityId: (cityId: string) => void;
+	setWeatherData: (weatherData: IWeather) => void;
 }> = createContext({
-  weatherData: {},
-  selectedCityId: "",
-  setSelectedCityId: (cityId) => {},
-  setWeatherData: (weatherData) => {},
+	weatherClient,
+	setSelectedCityId: (cityId) => {},
+	setWeatherData: (weatherData) => {},
 });
 
 export function WeatherContextProvider(props: { children: ReactNode }) {
-  const [weatherData, setWeatherData] = useState<IWeather | {}>({});
-  const [selectedCityId, setSelectedCityId] = useState("");
+	const [weatherData, setWeatherData] = useState<IWeather>();
+	const [selectedCityId, setSelectedCityId] = useState<string>();
 
-  const setSelectedCityIdHandler = (cityId: string) =>
-    setSelectedCityId(cityId);
+	const setSelectedCityIdHandler = (cityId: string) => setSelectedCityId(cityId);
 
-  const setWeatherDataHandler = (weatherData: IWeather) => {
-    setWeatherData(weatherData);
-  };
+	const setWeatherDataHandler = (weatherData: IWeather) => {
+		setWeatherData(weatherData);
+	};
 
-  const context = {
-    weatherData,
-    selectedCityId,
-    setSelectedCityId: setSelectedCityIdHandler,
-    setWeatherData: setWeatherDataHandler,
-  };
+	const context = {
+		weatherClient,
+		weatherData,
+		selectedCityId,
+		setSelectedCityId: setSelectedCityIdHandler,
+		setWeatherData: setWeatherDataHandler,
+	};
 
-  return (
-    <WeatherContext.Provider value={context}>
-      {props.children}
-    </WeatherContext.Provider>
-  );
+	return <WeatherContext.Provider value={context}>{props.children}</WeatherContext.Provider>;
 }
 
 export default WeatherContext;
